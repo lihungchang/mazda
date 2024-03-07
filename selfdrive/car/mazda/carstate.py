tomSg20/openpilot

no extra warning
Signed-off-by: Jafar Al-Gharaibeh <to.jafar@gmail.com>

selfdrive/car/mazda/carstate.py
@@ -3,7 +3,7 @@ from selfdrive.config import Conversions as CV
 from opendbc.can.can_define import CANDefine
 from opendbc.can.parser import CANParser
 from selfdrive.car.interfaces import CarStateBase
-from selfdrive.car.mazda.values import DBC, LKAS_LIMITS, GEN1
+from selfdrive.car.mazda.values import DBC, LKAS_LIMITS, GEN1, CAR
 
 class CarState(CarStateBase):
   def __init__(self, CP):
@@ -88,7 +88,12 @@ class CarState(CarStateBase):
     # Check if LKAS is disabled due to lack of driver torque when all other states indicate
     # it should be enabled (steer lockout). Don't warn until we actually get lkas active
     # and lose it again, i.e, after initial lkas activation
-    ret.steerWarning = self.lkas_allowed_speed and lkas_blocked
+
+    if self.CP.carFingerprint in [CAR.CX9_2021, CAR.CX5_2022]:
+      ret.steerWarning = False
+    else:
+      # On if no driver torque the last 5 seconds
+      ret.steerWarning = cp.vl["STEER_RATE"]["HANDS_OFF_5_SECONDS"] == 1
 
     self.acc_active_last = ret.cruiseState.enabled
 

lihungchang/mazda

selfdrive/car/mazda/carstate.py
@@ -3,7 +3,7 @@ from openpilot.common.conversions import Conversions as CV
 from opendbc.can.can_define import CANDefine
 from opendbc.can.parser import CANParser
 from openpilot.selfdrive.car.interfaces import CarStateBase
-from openpilot.selfdrive.car.mazda.values import DBC, LKAS_LIMITS, GEN1
+from openpilot.selfdrive.car.mazda.values import DBC, LKAS_LIMITS, GEN1, CAR
 
 class CarState(CarStateBase):
   def __init__(self, CP):
@@ -91,7 +91,12 @@ class CarState(CarStateBase):
     # Check if LKAS is disabled due to lack of driver torque when all other states indicate
     # it should be enabled (steer lockout). Don't warn until we actually get lkas active
     # and lose it again, i.e, after initial lkas activation
-    ret.steerFaultTemporary = self.lkas_allowed_speed and lkas_blocked
+
+    if self.CP.carFingerprint in [CAR.CX9_2021, CAR.CX5_2022]:
+      ret.steerFaultTemporary = False
+    else:
+      # On if no driver torque the last 5 seconds
+      ret.steerFaultTemporary = cp.vl["STEER_RATE"]["HANDS_OFF_5_SECONDS"] == 1
 
     self.acc_active_last = ret.cruiseState.enabled
 